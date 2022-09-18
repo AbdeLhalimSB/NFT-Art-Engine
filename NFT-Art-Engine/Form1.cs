73e3dd3923a95f4d1c969f48fa1c09d81fcba12c
AbdeLhalimSB <AbdeLhalimSB>
2022-09-18 20:14:27 +0100

    Add DNA

--- NFT-Art-Engine/Form1.cs
+++ NFT-Art-Engine/Form1.cs
@@ -24,7 +24,11 @@ namespace NFT_Art_Engine
         // Globale Dec --------------------------------------------------------------------------
         FolderBrowserDialog layersfolder = new FolderBrowserDialog();
         string[] layers;
-        string[] layers_details;
+        //string[] layers_details;
+        string[] target;
+        List<string> elemnts = new List<string>();
+        string krfhada;
+        List<string> DNA = new List<string>();
         //---------------------------------------------------------------------------------------
 
 
@@ -46,50 +50,63 @@ namespace NFT_Art_Engine
 
         private void Start_Click(object sender, EventArgs e)
         {
-            Image imageBackground = Image.FromFile("C:/Users/sahba/OneDrive/Desktop/Layers/Background/Background#10.png");
-            Image imageOverlay = Image.FromFile("C:/Users/sahba/OneDrive/Desktop/Layers/Men/Lotfi#10.png");
-            Image head = Image.FromFile("C:/Users/sahba/OneDrive/Desktop/Layers/Face/doubt on emoticon square face#10.png");
-
-            Image img = new Bitmap(imageBackground.Width, imageBackground.Height);
-            using (Graphics gr = Graphics.FromImage(img))
+            string dna_string = "";
+            for (int nft = 0; nft < ntf_count.Value; nft++)
             {
-                gr.DrawImage(imageBackground, new Point(0, 0));
-                gr.DrawImage(imageOverlay, new Point(0, 0));
-                gr.DrawImage(head, new Point(0, 0));
+                nftstart:
+                for(int i=0;i < layers.Length; i++)
+                {
+                    target = Directory.GetFiles(layers[i]);
+                    krfhada = target[randomer(0,target.Length)];
+                    elemnts.Add(krfhada);
+                }
+                Image imageBackground = Image.FromFile(elemnts[0]);
+
+                Image img = new Bitmap(imageBackground.Width, imageBackground.Height);
+                using (Graphics gr = Graphics.FromImage(img))
+                {
+                    gr.DrawImage(imageBackground, new Point(0, 0));
+                    for(int i = 1; i<elemnts.Count; i++)
+                    {
+                        gr.DrawImage(Image.FromFile(elemnts[i]), new Point(0, 0));
+                    }
+                }
+                for (int sc = 0; sc < elemnts.Count; sc++)
+                {
+                    dna_string += elemnts[sc];
+                }
+                
+                if (DNA.Contains(dna_string))
+                {
+                    logs.Text += "\n DNA Exist !!";
+                    elemnts.Clear();
+                    dna_string = "";
+                    goto nftstart;
+                }
+                else
+                {
+                    logs.Text += "\n DNA : "+dna_string;
+                    img.Save("C:/Users/sahba/OneDrive/Desktop/r/" + (nft + 1) + ".png", ImageFormat.Png);
+                    DNA.Add(dna_string);
+                }
+
+                elemnts.Clear();
+                dna_string = "";
             }
-            img.Save("C:/Users/sahba/OneDrive/Desktop/r/output.png", ImageFormat.Png);
+
+            logs.Text += "\n" + elemnts.Count;
+            logs.Text += "\n" + DNA.Count;
         }
 
 
 
         // Functions --------------------------------------------------------------------------
-        //private Bitmap MergeImages(IEnumerable<Bitmap> images)
-        //{
-        //    var enumerable = images as IList<Bitmap> ?? images.ToList();
-
-        //    var width = 0;
-        //    var height = 0;
-
-        //    foreach (var image in enumerable)
-        //    {
-        //        width += image.Width;
-        //        height = image.Height > height
-        //            ? image.Height
-        //            : height;
-        //    }
-
-        //    var bitmap = new Bitmap(width, height);
-        //    using (var g = Graphics.FromImage(bitmap))
-        //    {
-        //        var localWidth = 0;
-        //        foreach (var image in enumerable)
-        //        {
-        //            g.DrawImage(image, localWidth, 0);
-        //            localWidth += image.Width;
-        //        }
-        //    }
-        //    return bitmap;
-        //}
+        private int randomer(int start,int finish)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(start,finish);
+            return num;
+        }
 
 
 
